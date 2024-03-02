d86794489582d0dd50fd9a2d79e195e7218d5bb6
Salma Shakira Nurul Azmi <144917212+salmashakira@users.noreply.github.com>
2024-03-02 13:49:59 +0700

    Add files via upload

---
+++ SoalLatihan1/src/soallatihan1/SoalLatihan1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soallatihan1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author A S U S
+ */
+public class SoalLatihan1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Masukan nilai n: ");
+        int n = input.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("Saya senang belajar bahasa Java");
+        }
+
+    }
+    
+}
