0074fac83cfb7720a2a88d5ce52319cc35cb2563
Salma Shakira Nurul Azmi <144917212+salmashakira@users.noreply.github.com>
2024-03-02 13:50:49 +0700

    Add files via upload

---
+++ SoalLatihan3/src/soallatihan3/SoalLatihan3.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soallatihan3;
+
+import java.util.Scanner;
+/**
+ *
+ * @author A S U S
+ */
+public class SoalLatihan3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan bilangan a: ");
+        int a = input.nextInt();
+        System.out.print("Masukkan bilangan b: ");
+        int b = input.nextInt();
+
+        if (a < b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        
+        int fpb = hitungFPB(a, b);
+
+        System.out.println("Pembagi Bersama Terbesar dari " + a + " dan " + b + " adalah: " + fpb);
+
+        input.close();
+    }
+
+    public static int hitungFPB(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
